bbb710348176a7a918a4f03ec2ff2f1c98d9f433
Renaldi Dwi Saputra <renaldi31saputra@gmail.com>
2023-11-24 22:13:07 +0700

    sociabuzz

--- src/app/home/components/elements/Coffee.js
+++ src/app/home/components/elements/Coffee.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Coffee() {
   return (
     <>
-      <Link href='https://saweria.co/ryznoxy' target='_blank' aria-label='saweria'>
+      <Link href='https://sociabuzz.com/renmaruu/support' target='_blank' aria-label='sociabuzz'>
         <BiCoffee size={24} className="text-3xl"/>
       </Link>
     </>
